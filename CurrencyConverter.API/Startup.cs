ab5b276488fffbedc4f095671402aa57f6c69d30
Ricardo Antero <ricardo.antero@vm2.com.br>
2021-07-06 19:42:52 -0300

    Adicionado DB Context e Connection String

--- CurrencyConverter.API/Startup.cs
+++ CurrencyConverter.API/Startup.cs
@@ -1,15 +1,10 @@
+using CurrencyConverter.Infra.Data.Context;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace CurrencyConverter.API
 {
@@ -26,6 +21,12 @@ namespace CurrencyConverter.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<SqlDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ CurrencyConverter.Infra.Data/Context/SqlDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CurrencyConverter.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CurrencyConverter.Infra.Data.Context
+{
+    public class SqlDbContext : DbContext
+    {
+        public SqlDbContext(DbContextOptions<SqlDbContext> options) : base(options) { }
+
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Currencies> Currencies { get; set; }
+        public DbSet<Transactions> Transactions { get; set; }
+    }
+}
