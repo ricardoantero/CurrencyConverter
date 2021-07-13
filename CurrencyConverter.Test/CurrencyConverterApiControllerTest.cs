bebd632f8487b85acd820bbc708c4a3f39cf2184
Ricardo Antero <ricardo.antero@vm2.com.br>
2021-07-13 19:59:53 -0300

    Início do desenvolvimento de Teste Unitário Xunit

---
+++ CurrencyConverter.Test/CurrencyConverterApiControllerTest.cs
@@ -0,0 +1,34 @@
+﻿using CurrencyConverter.API.Controllers;
+using CurrencyConverter.Domain.Interfaces.Services;
+using CurrencyConverter.Infra.IoC.Api.Interface;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CurrencyConverter.Test
+{
+    public class CurrencyConverterApiControllerTest
+    {
+        readonly ITransactionsService _transactionsService;
+        readonly IUsersService _usersService;
+        readonly ICurrenciesService _currenciesService;
+        readonly IExchangeRatesApi _iexchangeratesapi;
+        readonly Serilog.ILogger _logger;
+
+        CurrencyConverterApiController _controller;
+        public CurrencyConverterApiControllerTest()
+        {
+            _controller = new CurrencyConverterApiController(_transactionsService,
+                                                             _usersService,
+                                                             _currenciesService,
+                                                             _iexchangeratesapi,
+                                                             _logger); 
+        }
+        [Theory]
+        [InlineData(1, "BRL", 1, new string[] { "EUR" })]
+        public void CurrencyTransaction_ReturnsOkResultAsync(int idUser, string OriginCurrency, decimal OriginValue, string[] DestinationCurrency)
+        {
+            // Act
+            // Assert
+        }
+    }
+}
